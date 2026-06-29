87a7fdcfada214d8b29ae568d4e699046aeb592c
aminerahik-dev <mohamedaminerahik@gmail.com>
2026-06-29 11:29:31 +0100

    updating links and images

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name        'FiveM-Admin-Menu'
+name        'fivem-admin-menu'
 author      'aminerahik-dev'
 version     '2.0.0'
 description 'Professional multi-framework admin menu — ESX, QBCore, Standalone'
-repository  'https://github.com/aminerahik-dev/FiveM-Admin-Menu'
+repository  'https://github.com/aminerahik-dev/fivem-admin-menu'
 
 
 shared_scripts {
